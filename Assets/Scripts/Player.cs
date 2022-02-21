3a0b8aee782bd3e3bb8e9652955e78b4fc8057b3
sj-jason-liu <sj.jason.liu@gmail.com>
2022-02-21 22:51:44 +0800

    Added 2D resources from Brackeys ' works; Added Player script to Player with basic movement function and jumping.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _movingSpeed = 3f;
+    [SerializeField]
+    private float _jumpForce = 5f;
+
+    private bool _isGrounded;
+
+    private Rigidbody2D _rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float move = horizontalInput * _movingSpeed;
+        _isGrounded = IsGrounded();
+
+        if(_isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+            }
+        }
+       
+        _rb.velocity = new Vector2(move, _rb.velocity.y);
+    }
+
+    bool IsGrounded() //check if Player is on the ground
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, 1 << 8);
+        if(hit.collider != null) //if Player is actually hit something
+        {
+            return true;
+        }
+        return false;
+    }
+}
